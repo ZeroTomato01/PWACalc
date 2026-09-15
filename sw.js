30cfde778c8eec30e0694dfbe3365f5dc5de90ba
ZeroTomato01 <146572680+ZeroTomato01@users.noreply.github.com>
2026-09-15 14:46:38 +0200

    Refactor caching strategy in service worker

--- sw.js
+++ sw.js
@@ -3,9 +3,7 @@ self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
-                "./",
-                "./src/manifest.json",
-                "./src/images/Syntess.png"
+                "./*"
             ]);
         })
     );
